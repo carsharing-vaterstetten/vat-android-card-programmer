31086b4dc94b8eca554904820369c4e80df0cda6
Jens Reimann <ctron@dentrassi.de>
2025-03-28 10:38:58 +0100

    chore: upgrade tools, and re-release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "de.dentrassi.vat.nfc.programmer"
         minSdk = 29
         targetSdk = 34
-        versionCode = 300
-        versionName = "0.3.0"
+        versionCode = 310
+        versionName = "0.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
